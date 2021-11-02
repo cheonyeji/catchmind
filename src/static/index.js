e46420f1aad13fc05b0974d0a6051f750d390843
1000yj <ssaltuck@gmail.com>
2021-11-02 14:30:01 +0900

    Set event listen (client)

--- src/static/index.js
+++ src/static/index.js
@@ -1,3 +1,17 @@
+// 클라이언트
 const socket = io("/");
 
-socket.on("hello", () => console.log("Somebody said Hello"));
+function handleMessageNoti(data) {
+  const { message, nickname } = data;
+  console.log(`${nickname} said ${message}`);
+}
+
+socket.on("messageNoti", handleMessageNoti);
+
+function sendMessage(message) {
+  socket.emit("newMessage", { message });
+}
+
+function setNickname(nickname) {
+  socket.emit("setNickname", { nickname });
+}
